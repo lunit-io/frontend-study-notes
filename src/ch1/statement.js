4d457c990b34607bb34efa969d10c19780d2e1d1
skpark-lunit <skpark@lunit.io>
2021-04-22 20:12:56 +0900

    amountFor 함수 추출하기

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -12,24 +12,7 @@ function statement(invoice, plays) {
   for (let perf of invoice.performances) {
     const play = plays[perf.playID]
 
-    let thisAmount = 0
-
-    switch(play.type) {
-      case 'tragedy':
-        thisAmount = 40000
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30)
-        }
-        break
-      case 'comedy':
-        thisAmount = 30000
-        if (perf.audience > 20) {
-          thisAmount += 300 * perf.audience
-        }
-        break
-      default:
-        throw new Error(`unknown type: ${play.type}`)
-    }
+    let thisAmount = amountFor(perf, play)
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0)
@@ -44,6 +27,28 @@ function statement(invoice, plays) {
   result += `Amount owed is ${format(totalAmount / 100)}\n`
   result += `You earned ${volumeCredits} credits\n`
   return result
+}
+
+function amountFor(perf, play) {
+  let thisAmount = 0
+
+  switch(play.type) {
+    case 'tragedy':
+      thisAmount = 40000
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30)
+      }
+      break
+    case 'comedy':
+      thisAmount = 30000
+      if (perf.audience > 20) {
+        thisAmount += 300 * perf.audience
+      }
+      break
+    default:
+      throw new Error(`unknown type: ${play.type}`)
+  }
+  return thisAmount
 }
 
 export default statement
