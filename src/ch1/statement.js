8f1a830042404d26765042aacab56098eb0bda9f
skpark-lunit <skpark@lunit.io>
2021-04-22 20:22:09 +0900

    임시 변수를 Query로 바꾸기

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -1,54 +1,64 @@
-function statement(invoice, plays) {
-  let totalAmount = 0
-  let volumeCredits = 0
-  let result = `Statement for ${invoice.customer}\n`
+import invoices from "./data/invoices.json";
+import plays from "./data/plays.json";
 
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2
-  }).format
+const invoice = invoices[0];
+
+function playFor(aPerformance) {
+  return plays[aPerformance.playID];
+}
+
+function statement() {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `Statement for ${invoice.customer}\n`;
+
+  const format = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID]
+    const play = playFor(perf, plays);
 
-    let thisAmount = amountFor(perf, play)
+    let thisAmount = amountFor(perf, play);
 
     // add volume credits
-    volumeCredits += Math.max(perf.audience - 30, 0)
+    volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
-    if ('comedy' === play.type)
-      volumeCredits += Math.floor(perf.audience / 5)
+    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
 
     // print line for this order
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`
-    totalAmount += thisAmount
+    result += ` ${play.name}: ${format(thisAmount / 100)} (${
+      perf.audience
+    } seats)\n`;
+    totalAmount += thisAmount;
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`
-  result += `You earned ${volumeCredits} credits\n`
-  return result
+  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `You earned ${volumeCredits} credits\n`;
+  return result;
 }
 
 function amountFor(aPerformance, play) {
-  let result = 0
+  let result = 0;
 
-  switch(play.type) {
-    case 'tragedy':
-      result = 40000
+  switch (play.type) {
+    case "tragedy":
+      result = 40000;
       if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30)
+        result += 1000 * (aPerformance.audience - 30);
       }
-      break
-    case 'comedy':
-      result = 30000
+      break;
+    case "comedy":
+      result = 30000;
       if (aPerformance.audience > 20) {
-        result += 300 * aPerformance.audience
+        result += 300 * aPerformance.audience;
       }
-      break
+      break;
     default:
-      throw new Error(`unknown type: ${play.type}`)
+      throw new Error(`unknown type: ${play.type}`);
   }
-  return result
+  return result;
 }
 
-export default statement
+export default statement;
