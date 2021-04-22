45d6087abf335e60eb2d17f4167fa9c08793b33b
skpark-lunit <skpark@lunit.io>
2021-04-22 21:00:12 +0900

    totalAmount 추출하여 인라인 하기

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -16,7 +16,6 @@ function usd(aNumber) {
 }
 
 function statement() {
-  let totalAmount = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
@@ -24,20 +23,29 @@ function statement() {
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${
       perf.audience
     } seats)\n`;
-    totalAmount += amountFor(perf);
   }
 
-  result += `Amount owed is ${usd(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(totalAmount() / 100)}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
 }
 
-function totalVolumeCredits() {
-  let volumeCredits = 0;
+function totalAmount() {
+  let result = 0;
+
   for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
+    result += amountFor(perf);
   }
-  return volumeCredits;
+
+  return result;
+}
+
+function totalVolumeCredits() {
+  let result = 0;
+  for (let perf of invoice.performances) {
+    result += volumeCreditsFor(perf);
+  }
+  return result;
 }
 
 function volumeCreditsFor(aPerformance) {
