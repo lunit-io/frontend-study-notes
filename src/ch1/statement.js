f3b865c0ad0c92b66bda7ab63c4605147ec77ce9
skpark-lunit <skpark@lunit.io>
2021-04-22 20:56:16 +0900

    volumeCredits 변수 인라인 하기

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -27,10 +27,8 @@ function statement() {
     totalAmount += amountFor(perf);
   }
 
-  let volumeCredits = totalVolumeCredits();
-
   result += `Amount owed is ${usd(totalAmount / 100)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
+  result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
 }
 
