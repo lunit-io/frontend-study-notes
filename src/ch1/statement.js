62c08fe7fb7e2e34f9455e6797699a5bc3373873
skpark-lunit <skpark@lunit.io>
2021-04-22 21:12:30 +0900

    중간 데이터 사용으로 매개변수 제거하기

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -1,11 +1,15 @@
 function statement(invoice, plays) {
-  return renderPlainText(invoice, plays);
+  const statementData = {};
+  statementData.customer = invoice.customer; // 고객 데이터를 중간 데이터로 옮김
+  statementData.performances = invoice.performances;
+
+  return renderPlainText(statementData, plays); // 중간 데이터 구조를 인수로 전달
 }
 
-function renderPlainText(invoice, plays) {
-  let result = `Statement for ${invoice.customer}\n`;
+function renderPlainText(data, plays) {
+  let result = `Statement for ${data.customer}\n`;
 
-  for (let perf of invoice.performances) {
+  for (let perf of data.performances) {
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${
       perf.audience
     } seats)\n`;
@@ -18,7 +22,7 @@ function renderPlainText(invoice, plays) {
   function totalAmount() {
     let result = 0;
 
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += amountFor(perf);
     }
 
@@ -39,7 +43,7 @@ function renderPlainText(invoice, plays) {
 
   function totalVolumeCredits() {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += volumeCreditsFor(perf);
     }
     return result;
