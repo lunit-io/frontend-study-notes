1c10b560abf7c2b0f1947bab7be1af244063c222
skpark-lunit <skpark@lunit.io>
2021-04-22 21:21:22 +0900

    중간 데이터를 사용하여 amountFor() 중목 호출 제거하기

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -8,11 +8,34 @@ function statement(invoice, plays) {
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance); // 얕은 복사 수행
     result.play = playFor(result);
+    result.amount = amountFor(result);
     return result;
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
+  }
+
+  function amountFor(aPerformance) {
+    let result = 0;
+
+    switch (aPerformance.play.type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 300 * aPerformance.audience;
+        }
+        break;
+      default:
+        throw new Error(`unknown type: ${aPerformance.play.type}`);
+    }
+    return result;
   }
 }
 
@@ -20,7 +43,7 @@ function renderPlainText(data) {
   let result = `Statement for ${data.customer}\n`;
 
   for (let perf of data.performances) {
-    result += ` ${perf.play.name}: ${usd(amountFor(perf) / 100)} (${
+    result += ` ${perf.play.name}: ${usd(perf.amount / 100)} (${
       perf.audience
     } seats)\n`;
   }
@@ -33,7 +56,7 @@ function renderPlainText(data) {
     let result = 0;
 
     for (let perf of data.performances) {
-      result += amountFor(perf);
+      result += perf.amount;
     }
 
     return result;
@@ -62,29 +85,6 @@ function renderPlainText(data) {
     if ("comedy" === aPerformance.play.type)
       result += Math.floor(aPerformance.audience / 5);
 
-    return result;
-  }
-
-  function amountFor(aPerformance) {
-    let result = 0;
-
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 300 * aPerformance.audience;
-        }
-        break;
-      default:
-        throw new Error(`unknown type: ${aPerformance.play.type}`);
-    }
     return result;
   }
 }
