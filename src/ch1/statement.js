26535b22c534c0234d06016b4fa37919398fbd26
skpark-lunit <skpark@lunit.io>
2021-04-22 20:42:33 +0900

    thisAmount에 인라인 변수 사용하기

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -19,8 +19,6 @@ function statement() {
   }).format;
 
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf);
-
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
@@ -28,10 +26,10 @@ function statement() {
       volumeCredits += Math.floor(perf.audience / 5);
 
     // print line for this order
-    result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${
+    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
       perf.audience
     } seats)\n`;
-    totalAmount += thisAmount;
+    totalAmount += amountFor(perf);
   }
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
