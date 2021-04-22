d5898b25ebc22a47e833726e76c524375a31d337
skpark-lunit <skpark@lunit.io>
2021-04-22 21:45:07 +0900

    createStatementData 파일 분리하기

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -1,63 +1,7 @@
+import createStatementData from "./createStatementData";
+
 function statement(invoice, plays) {
-  const statementData = {};
-  statementData.customer = invoice.customer; // 고객 데이터를 중간 데이터로 옮김
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-
-  return renderPlainText(statementData); // 중간 데이터 구조를 인수로 전달
-
-  function enrichPerformance(aPerformance) {
-    const result = Object.assign({}, aPerformance); // 얕은 복사 수행
-    result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
-    return result;
-
-    function playFor(aPerformance) {
-      return plays[aPerformance.playID];
-    }
-  }
-}
-
-function amountFor(aPerformance) {
-  let result = 0;
-
-  switch (aPerformance.play.type) {
-    case "tragedy":
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy":
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 300 * aPerformance.audience;
-      }
-      break;
-    default:
-      throw new Error(`unknown type: ${aPerformance.play.type}`);
-  }
-  return result;
-}
-
-function volumeCreditsFor(aPerformance) {
-  let result = 0;
-  result += Math.max(aPerformance.audience - 30, 0);
-
-  if ("comedy" === aPerformance.play.type)
-    result += Math.floor(aPerformance.audience / 5);
-
-  return result;
-}
-
-function totalAmount(data) {
-  return data.performances.reduce((total, p) => total + p.amount, 0);
-}
-
-function totalVolumeCredits(data) {
-  return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
+  return renderPlainText(createStatementData(invoice, plays));
 }
 
 function renderPlainText(data) {
