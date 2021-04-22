ed3b54858dcf591dcf4cc7c91a6d177801e6945b
skpark-lunit <skpark@lunit.io>
2021-04-22 20:25:02 +0900

    인라인 변수 사용하기

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -19,17 +19,16 @@ function statement() {
   }).format;
 
   for (let perf of invoice.performances) {
-    const play = playFor(perf, plays);
-
-    let thisAmount = amountFor(perf, play);
+    let thisAmount = amountFor(perf, playFor(perf));
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" === playFor(perf).type)
+      volumeCredits += Math.floor(perf.audience / 5);
 
     // print line for this order
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${
+    result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${
       perf.audience
     } seats)\n`;
     totalAmount += thisAmount;
