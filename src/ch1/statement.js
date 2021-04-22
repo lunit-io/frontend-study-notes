a51d74086422bcd01f5b9deb16c48a542e2e97b6
skpark-lunit <skpark@lunit.io>
2021-04-22 20:48:28 +0900

    format 함수 추출하기

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -7,16 +7,18 @@ function playFor(aPerformance) {
   return plays[aPerformance.playID];
 }
 
+function format(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber);
+}
+
 function statement() {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format;
 
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
