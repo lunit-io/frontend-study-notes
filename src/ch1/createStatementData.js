f85b5ee8b2721261672e02da69912d155c5336e3
skpark-lunit <skpark@lunit.io>
2021-04-22 22:15:15 +0900

    서브클래스에서 volumeCredits() 처리

--- src/ch1/createStatementData.js
+++ src/ch1/createStatementData.js
@@ -42,13 +42,7 @@ class PerformanceCalculator {
   }
 
   get volumeCredits() {
-    let result = 0
-    result += Math.max(this.performance.audience - 30, 0)
-  
-    if ('comedy' === this.play.type)
-    result += Math.floor(this.performance.audience / 5)
-  
-    return result
+    return Math.max(this.performance.audience - 30, 0)
   }
 }
 
@@ -78,5 +72,9 @@ class ComedyCalculator extends PerformanceCalculator {
       result += 300 * this.performance.audience
     }
     return result
+  }
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5)
   }
 }
