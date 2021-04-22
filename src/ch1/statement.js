f807a098de39a4313909ba60d6d89c34d082447c
skpark-lunit <skpark@lunit.io>
2021-04-22 20:46:41 +0900

    volumeCreditsFor 함수 추출하기

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -19,11 +19,7 @@ function statement() {
   }).format;
 
   for (let perf of invoice.performances) {
-    // add volume credits
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
-    if ("comedy" === playFor(perf).type)
-      volumeCredits += Math.floor(perf.audience / 5);
+    volumeCredits += volumeCreditsFor(perf);
 
     // print line for this order
     result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
@@ -33,6 +29,16 @@ function statement() {
   }
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
+  return result;
+}
+
+function volumeCreditsFor(aPerformance) {
+  let result = 0;
+  result += Math.max(aPerformance.audience - 30, 0);
+
+  if ("comedy" === playFor(aPerformance).type)
+    result += Math.floor(aPerformance.audience / 5);
+
   return result;
 }
 
