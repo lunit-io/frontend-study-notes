ecc8b580a26d6450f60764dcc1978a7a7fa251aa
skpark-lunit <skpark@lunit.io>
2021-04-22 21:06:14 +0900

    함수를 데이터 처리함수와 렌더링 함수로 분리하기

--- src/ch1/statement.test.js
+++ src/ch1/statement.test.js
@@ -1,6 +1,6 @@
-import statement from './statement'
-import invoice from './data/invoices.json'
-import plays from './data/plays.json'
+import statement from "./statement";
+import invoices from "./data/invoices.json";
+import plays from "./data/plays.json";
 
 const result = `Statement for BigCo
  Hamlet: $650.00 (55 seats)
@@ -8,8 +8,8 @@ const result = `Statement for BigCo
  Othello: $500.00 (40 seats)
 Amount owed is $1,555.00
 You earned 47 credits
-`
+`;
 
-test('statement result', () => {
-  expect(statement(invoice[0], plays)).toBe(result)
-})
+test("statement result", () => {
+  expect(statement(invoices[0], plays)).toBe(result);
+});
