1f9ea3b8c0d0e945e9c40841540a15e4e87c7ff4
skpark-lunit <skpark@lunit.io>
2021-04-22 21:40:36 +0900

    함수 위치 오류 수정

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -13,69 +13,51 @@ function statement(invoice, plays) {
     result.amount = amountFor(result);
     result.volumeCredits = volumeCreditsFor(result);
     return result;
-  }
 
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance) {
-    let result = 0;
-
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 300 * aPerformance.audience;
-        }
-        break;
-      default:
-        throw new Error(`unknown type: ${aPerformance.play.type}`);
+    function playFor(aPerformance) {
+      return plays[aPerformance.playID];
     }
-    return result;
   }
+}
 
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
+function amountFor(aPerformance) {
+  let result = 0;
 
-    if ("comedy" === aPerformance.play.type)
-      result += Math.floor(aPerformance.audience / 5);
-
-    return result;
+  switch (aPerformance.play.type) {
+    case "tragedy":
+      result = 40000;
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
+      }
+      break;
+    case "comedy":
+      result = 30000;
+      if (aPerformance.audience > 20) {
+        result += 300 * aPerformance.audience;
+      }
+      break;
+    default:
+      throw new Error(`unknown type: ${aPerformance.play.type}`);
   }
+  return result;
+}
 
-  function totalAmount() {
-    let result = 0;
+function volumeCreditsFor(aPerformance) {
+  let result = 0;
+  result += Math.max(aPerformance.audience - 30, 0);
 
-    for (let perf of statementData.performances) {
-      result += perf.amount;
-    }
+  if ("comedy" === aPerformance.play.type)
+    result += Math.floor(aPerformance.audience / 5);
 
-    return result;
-  }
+  return result;
+}
 
-  function totalVolumeCredits() {
-    let result = 0;
-    for (let perf of statementData.performances) {
-      result += perf.volumeCredits;
-    }
-    return result;
-  }
+function totalAmount(data) {
+  return data.performances.reduce((total, p) => total + p.amount, 0);
+}
 
-  function totalAmount(data) {
-    return data.performances.reduce((total, p) => total + p.amount, 0);
-  }
-
-  function totalVolumeCredits(data) {
-    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
-  }
+function totalVolumeCredits(data) {
+  return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
 }
 
 function renderPlainText(data) {
