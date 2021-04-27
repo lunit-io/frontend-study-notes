553f298354f3dd9de939711cd6b3fa0f88db2d44
Jiyeon Seo <jyseo@lunit.io>
2021-04-27 23:13:01 +0900

    volumeCredits 변수 인라인함수화

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -15,10 +15,8 @@ function statement() {
     totalAmount += amountFor(perf);
   }
 
-  let volumeCredits = totalVolumeCredits();
-
   result += `Amount owed is ${usd(totalAmount)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
+  result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
 }
 
