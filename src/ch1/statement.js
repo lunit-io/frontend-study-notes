ea7dab0185ee9a96f66a19f4a0e7d71c10837f85
Jiyeon Seo <jyseo@lunit.io>
2021-04-27 23:00:03 +0900

    volumeCreditsFor() 의 변수명 명확한 이름으로 변경

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -34,14 +34,15 @@ function playFor(aPerformance) {
   return plays[aPerformance.playID];
 }
 
-function volumeCreditsFor(perf) {
-  let volumeCredits = 0;
-  volumeCredits += Math.max(perf.audience - 30, 0);
+function volumeCreditsFor(aPerformance) {
+  let result = 0;
+  result += Math.max(aPerformance.audience - 30, 0);
   // add extra credit for every ten comedy attendees
-  if ("comedy" === playFor(perf).type)
-    volumeCredits += Math.floor(perf.audience / 5);
+  if ("comedy" === playFor(aPerformance).type) {
+    result += Math.floor(aPerformance.audience / 5);
+  }
 
-  return volumeCredits;
+  return result;
 }
 
 function amountFor(aPerformance) {
