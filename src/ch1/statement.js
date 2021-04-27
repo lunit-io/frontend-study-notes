f58baff3148bc479a47856ce3a2bdaeff4faca32
Jiyeon Seo <jyseo@lunit.io>
2021-04-27 23:14:58 +0900

    totalAmount 처리 로직만 따로 분리

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -4,7 +4,6 @@ import plays from "./data/plays.json";
 const invoice = invoices[0];
 
 function statement() {
-  let totalAmount = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
@@ -12,6 +11,10 @@ function statement() {
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
+  }
+
+  let totalAmount = 0;
+  for (let perf of invoice.performances) {
     totalAmount += amountFor(perf);
   }
 
