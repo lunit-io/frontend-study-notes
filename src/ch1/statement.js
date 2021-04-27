5d703ddf3101991501f97c56e7ac9c12347562bf
Jiyeon Seo <jyseo@lunit.io>
2021-04-27 22:35:24 +0900

    amountFor 함수 분리

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -17,24 +17,7 @@ function statement() {
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
 
-    let thisAmount = 0;
-
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 300 * perf.audience;
-        }
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
+    let thisAmount = amountFor(perf, play);
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -53,3 +36,26 @@ function statement() {
 }
 
 export default statement;
+
+function amountFor(perf, play) {
+  let thisAmount = 0;
+
+  switch (play.type) {
+    case "tragedy":
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+    case "comedy":
+      thisAmount = 30000;
+      if (perf.audience > 20) {
+        thisAmount += 300 * perf.audience;
+      }
+      break;
+    default:
+      throw new Error(`unknown type: ${play.type}`);
+  }
+
+  return thisAmount;
+}
