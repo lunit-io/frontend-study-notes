a9ab4db20b3c5ec45ef47f1fb72fe0ff89939b7c
Jiyeon Seo <jyseo@lunit.io>
2021-04-28 02:02:01 +0900

    amountFor() 함수 인라인화 하고 기존 함수 삭제

--- src/ch1/createStatementData.js
+++ src/ch1/createStatementData.js
@@ -13,18 +13,13 @@ function createStatementData(invoice, plays) {
     );
     const result = Object.assign({}, aPerformance);
     result.play = calculator.play;
-    result.amount = amountFor(result);
+    result.amount = calculator.amount;
     result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance) {
-    return new PerformanceCalculator(aPerformance, playFor(aPerformance))
-      .amount;
   }
 
   function volumeCreditsFor(aPerformance) {
