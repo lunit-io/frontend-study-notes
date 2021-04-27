8f72ca67a679bbbc416157af5a7797b4a98e500e
Jiyeon Seo <jyseo@lunit.io>
2021-04-27 22:38:54 +0900

    amountFor 함수 내 변수 이름을 명확하게 변경

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -37,25 +37,25 @@ function statement() {
 
 export default statement;
 
-function amountFor(perf, play) {
-  let thisAmount = 0;
+function amountFor(aPerformance, play) {
+  let result = 0;
 
   switch (play.type) {
     case "tragedy":
-      thisAmount = 40000;
-      if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
+      result = 40000;
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
       }
       break;
     case "comedy":
-      thisAmount = 30000;
-      if (perf.audience > 20) {
-        thisAmount += 300 * perf.audience;
+      result = 30000;
+      if (aPerformance.audience > 20) {
+        result += 300 * aPerformance.audience;
       }
       break;
     default:
       throw new Error(`unknown type: ${play.type}`);
   }
 
-  return thisAmount;
+  return result;
 }
