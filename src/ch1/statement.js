caca35bee95a769c570ca06e1dfe88eda41b4c88
Jiyeon Seo <jyseo@lunit.io>
2021-04-27 22:45:48 +0900

    statement() 내 play 변수를 인라인 함수화

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -15,17 +15,16 @@ function statement() {
   }).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-
-    let thisAmount = amountFor(perf, play);
+    let thisAmount = amountFor(perf, playFor(perf));
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" === playFor(perf).type)
+      volumeCredits += Math.floor(perf.audience / 5);
 
     // print line for this order
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${
+    result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${
       perf.audience
     } seats)\n`;
     totalAmount += thisAmount;
@@ -36,6 +35,10 @@ function statement() {
 }
 
 export default statement;
+
+function playFor(aPerformance) {
+  return plays[aPerformance.playID];
+}
 
 function amountFor(aPerformance, play) {
   let result = 0;
