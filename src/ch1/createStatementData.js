c7c4cd8b276fe47c0963346059e24c7177c3f343
Jiyeon Seo <jyseo@lunit.io>
2021-04-28 01:40:53 +0900

    계산기 클래스에 연극 정보 전달

--- src/ch1/createStatementData.js
+++ src/ch1/createStatementData.js
@@ -7,9 +7,12 @@ function createStatementData(invoice, plays) {
   return statementData;
 
   function enrichPerformance(aPerformance) {
-    const calculator = new PerformanceCalculator(aPerformance);
+    const calculator = new PerformanceCalculator(
+      aPerformance,
+      playFor(aPerformance)
+    );
     const result = Object.assign({}, aPerformance);
-    result.play = playFor(result);
+    result.play = calculator.play;
     result.amount = amountFor(result);
     result.volumeCredits = volumeCreditsFor(result);
     return result;
@@ -65,7 +68,8 @@ function createStatementData(invoice, plays) {
 export default createStatementData;
 
 class PerformanceCalculator {
-  constructor(aPerformance) {
+  constructor(aPerformance, aPlay) {
     this.performance = aPerformance;
+    this.play = aPlay;
   }
 }
