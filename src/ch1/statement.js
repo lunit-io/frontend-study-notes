afd77015a637b8832a641f2dcd6e783ab0b74554
Jiyeon Seo <jyseo@lunit.io>
2021-04-27 23:30:55 +0900

    statement() 내용을 renderPlainText() 별도 함수로 추출
    이전에 선언했던 다른 함수들도 renderPlainText() 의 내부함수화

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -4,6 +4,12 @@ import plays from "./data/plays.json";
 const invoice = invoices[0];
 
 function statement() {
+  return renderPlainText(invoice, plays);
+}
+
+export default statement;
+
+function renderPlainText(invoice, plays) {
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
@@ -16,70 +22,68 @@ function statement() {
   result += `Amount owed is ${usd(totalAmount())}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
-}
 
-export default statement;
-
-function playFor(aPerformance) {
-  return plays[aPerformance.playID];
-}
-
-function usd(aNumber) {
-  return new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format(aNumber / 100);
-}
-
-// 나는 몹시 불편하다.. 전역 객체도 불편, 변수 인라인 함수화도 몹시 불편..
-function totalAmount() {
-  // 왜 apple sauce? do you like apple sauce?
-  let result = 0;
-  for (let perf of invoice.performances) {
-    result += amountFor(perf);
-  }
-  return result;
-}
-
-function totalVolumeCredits() {
-  let result = 0;
-  for (let perf of invoice.performances) {
-    result += volumeCreditsFor(perf);
-  }
-  return result;
-}
-
-function volumeCreditsFor(aPerformance) {
-  let result = 0;
-  result += Math.max(aPerformance.audience - 30, 0);
-  // add extra credit for every ten comedy attendees
-  if ("comedy" === playFor(aPerformance).type) {
-    result += Math.floor(aPerformance.audience / 5);
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
   }
 
-  return result;
-}
-
-function amountFor(aPerformance) {
-  let result = 0;
-
-  switch (playFor(aPerformance).type) {
-    case "tragedy":
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy":
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 300 * aPerformance.audience;
-      }
-      break;
-    default:
-      throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+  function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2,
+    }).format(aNumber / 100);
   }
 
-  return result;
+  // 나는 몹시 불편하다.. 전역 객체도 불편, 변수 인라인 함수화도 몹시 불편..
+  function totalAmount() {
+    // 왜 apple sauce? do you like apple sauce?
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += amountFor(perf);
+    }
+    return result;
+  }
+
+  function totalVolumeCredits() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += volumeCreditsFor(perf);
+    }
+    return result;
+  }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    // add extra credit for every ten comedy attendees
+    if ("comedy" === playFor(aPerformance).type) {
+      result += Math.floor(aPerformance.audience / 5);
+    }
+
+    return result;
+  }
+
+  function amountFor(aPerformance) {
+    let result = 0;
+
+    switch (playFor(aPerformance).type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 300 * aPerformance.audience;
+        }
+        break;
+      default:
+        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+    }
+
+    return result;
+  }
 }
