436c15aad1cdd441cc215d05c875163d78297707
Jiyeon Seo <jyseo@lunit.io>
2021-04-27 23:10:43 +0900

    volumeCredits 처리 로직을 totalVolumeCredits() 함수로 따로 추출

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -15,10 +15,7 @@ function statement() {
     totalAmount += amountFor(perf);
   }
 
-  let volumeCredits = 0;
-  for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-  }
+  let volumeCredits = totalVolumeCredits();
 
   result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
@@ -37,6 +34,14 @@ function usd(aNumber) {
     currency: "USD",
     minimumFractionDigits: 2,
   }).format(aNumber / 100);
+}
+
+function totalVolumeCredits() {
+  let volumeCredits = 0;
+  for (let perf of invoice.performances) {
+    volumeCredits += volumeCreditsFor(perf);
+  }
+  return volumeCredits;
 }
 
 function volumeCreditsFor(aPerformance) {
