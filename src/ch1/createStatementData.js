bec008b91684f6192b70dbe1142679c17b8d3156
Jiyeon Seo <jyseo@lunit.io>
2021-04-28 01:50:44 +0900

    amount 계신식이 책과 달라서 수정 | calculator class 에 amount 함수 생성

--- src/ch1/createStatementData.js
+++ src/ch1/createStatementData.js
@@ -35,8 +35,9 @@ function createStatementData(invoice, plays) {
       case "comedy":
         result = 30000;
         if (aPerformance.audience > 20) {
-          result += 300 * aPerformance.audience;
+          result += 10000 + 500 * (aPerformance.audience - 20);
         }
+        result += 300 * aPerformance.audience;
         break;
       default:
         throw new Error(`unknown type: ${aPerformance.play.type}`);
@@ -71,5 +72,29 @@ class PerformanceCalculator {
   constructor(aPerformance, aPlay) {
     this.performance = aPerformance;
     this.play = aPlay;
+  }
+
+  get amount() {
+    let result = 0;
+
+    switch (this.play.type) {
+      case "tragedy":
+        result = 40000;
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${this.play.type}`);
+    }
+
+    return result;
   }
 }
