a9206bd6de6aae130d430c4049087b535a80ecf4
Jiyeon Seo <jyseo@lunit.io>
2021-04-28 01:03:22 +0900

    html render 함수 추가

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -8,7 +8,9 @@ function statement() {
   return renderPlainText(createStatementData(invoice, plays));
 }
 
-export default statement;
+function htmlStatement() {
+  return renderHtml(createStatementData(invoice, plays));
+}
 
 function renderPlainText(data) {
   let result = `Statement for ${data.customer}\n`;
@@ -21,6 +23,20 @@ function renderPlainText(data) {
 
   result += `Amount owed is ${usd(data.totalAmount)}\n`;
   result += `You earned ${data.totalVolumeCredits} credits\n`;
+  return result;
+}
+
+function renderHtml(data) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+  for (let perf of data.performances) {
+    result += `  <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
+  }
+  result += "</table>\n";
+  result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
   return result;
 }
 
