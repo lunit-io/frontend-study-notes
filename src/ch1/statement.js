f947a587183831a7e37016194308d11df060365e
Jiyeon Seo <jyseo@lunit.io>
2021-04-28 00:41:51 +0900

    volumeCreditsFor() 를 statement 로 옮기고 데이터 입력

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -13,6 +13,7 @@ function statement() {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
     result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
 
@@ -38,6 +39,17 @@ function statement() {
         break;
       default:
         throw new Error(`unknown type: ${aPerformance.play.type}`);
+    }
+
+    return result;
+  }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    // add extra credit for every ten comedy attendees
+    if ("comedy" === aPerformance.play.type) {
+      result += Math.floor(aPerformance.audience / 5);
     }
 
     return result;
@@ -78,19 +90,8 @@ function renderPlainText(data) {
   function totalVolumeCredits() {
     let result = 0;
     for (let perf of data.performances) {
-      result += volumeCreditsFor(perf);
+      result += perf.volumeCredits;
     }
-    return result;
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
-    if ("comedy" === aPerformance.play.type) {
-      result += Math.floor(aPerformance.audience / 5);
-    }
-
     return result;
   }
 }
