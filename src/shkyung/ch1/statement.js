88c745fcd102e37e156742aac5482a886e188145
sunny kyung <shkyung@lunit.io>
2021-04-29 00:36:35 +0900

    remove thisAmount using variable inline

--- src/shkyung/ch1/statement.js
+++ src/shkyung/ch1/statement.js
@@ -15,18 +15,16 @@ function statement() {
   }).format;
 
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf)
-
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
     if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
     // print line for this order
-    result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${
+    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
       perf.audience
     } seats)\n`;
-    totalAmount += thisAmount;
+    totalAmount += amountFor(perf);
   }
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
