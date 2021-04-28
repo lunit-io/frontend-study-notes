ee5d2dd3f73cfee20675a53008846ce92d79b228
sunny kyung <shkyung@lunit.io>
2021-04-28 22:58:29 +0900

    extract switch statement and make it nested function

--- src/shkyung/ch1/statement.js
+++ src/shkyung/ch1/statement.js
@@ -4,6 +4,28 @@ import plays from "./data/plays.json";
 const invoice = invoices[0];
 
 function statement() {
+  function amountFor(aPerformance, play) {
+    let result = 0;
+    switch (play.type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${play.type}`);
+    }
+    return result;
+  }
+
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
@@ -16,26 +38,7 @@ function statement() {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-
-    let thisAmount = 0;
-
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
+    let thisAmount = amountFor(perf, play)
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
