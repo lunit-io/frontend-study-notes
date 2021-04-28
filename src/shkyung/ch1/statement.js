174f76c02ce04c351bb408d4fd25faf974b123ce
sunny kyung <shkyung@lunit.io>
2021-04-29 00:21:38 +0900

    remove play parameter using variable inline

--- src/shkyung/ch1/statement.js
+++ src/shkyung/ch1/statement.js
@@ -15,16 +15,15 @@ function statement() {
   }).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = amountFor(perf, play)
+    let thisAmount = amountFor(perf)
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
     // print line for this order
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${
+    result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${
       perf.audience
     } seats)\n`;
     totalAmount += thisAmount;
@@ -34,9 +33,13 @@ function statement() {
   return result;
 }
 
-function amountFor(aPerformance, play) {
+function playFor(aPerformance) {
+  return plays[aPerformance.playID]
+}
+
+function amountFor(aPerformance) {
   let result = 0;
-  switch (play.type) {
+  switch (playFor(aPerformance).type) {
     case "tragedy":
       result = 40000;
       if (aPerformance.audience > 30) {
@@ -51,7 +54,7 @@ function amountFor(aPerformance, play) {
       result += 300 * aPerformance.audience;
       break;
     default:
-      throw new Error(`unknown type: ${play.type}`);
+      throw new Error(`unknown type: ${playFor(aPerformance).type}`);
   }
   return result;
 }
