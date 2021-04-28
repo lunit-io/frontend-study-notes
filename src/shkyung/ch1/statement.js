e6e2e6a5e33a78ef98614275242e8c973ac9aa9a
sunny kyung <shkyung@lunit.io>
2021-04-28 23:48:26 +0900

    move amountFor() outside statement()

--- src/shkyung/ch1/statement.js
+++ src/shkyung/ch1/statement.js
@@ -4,28 +4,6 @@ import plays from "./data/plays.json";
 const invoice = invoices[0];
 
 function statement() {
-  function amountFor(aPerformance, play) {
-    let result = 0;
-    switch (play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
-    return result;
-  }
-
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
@@ -53,6 +31,28 @@ function statement() {
   }
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
+  return result;
+}
+
+function amountFor(aPerformance, play) {
+  let result = 0;
+  switch (play.type) {
+    case "tragedy":
+      result = 40000;
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
+      }
+      break;
+    case "comedy":
+      result = 30000;
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
+      }
+      result += 300 * aPerformance.audience;
+      break;
+    default:
+      throw new Error(`unknown type: ${play.type}`);
+  }
   return result;
 }
 
