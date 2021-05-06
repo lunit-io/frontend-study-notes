6ae332d24e09dc202928955dbab5aac629175092
sunny kyung <shkyung@lunit.io>
2021-05-06 15:50:30 +0900

    remove volumeCredits variable

--- src/shkyung/ch1/statement.js
+++ src/shkyung/ch1/statement.js
@@ -5,21 +5,27 @@ const invoice = invoices[0];
 
 function statement() {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf)
     // print line for this order
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
     totalAmount += amountFor(perf);
   }
+
   result += `Amount owed is ${usd(totalAmount)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
+  result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
 
+  function totalVolumeCredits() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += volumeCreditsFor(perf)
+    }
+    return result
+  }
 
   function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {
