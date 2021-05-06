a916e1e8724034d89aaf412c61b90d2134f3ca44
sunny kyung <shkyung@lunit.io>
2021-05-06 16:00:15 +0900

    remove totalAmount variable

--- src/shkyung/ch1/statement.js
+++ src/shkyung/ch1/statement.js
@@ -4,7 +4,6 @@ import plays from "./data/plays.json";
 const invoice = invoices[0];
 
 function statement() {
-  let totalAmount = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
@@ -12,12 +11,19 @@ function statement() {
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
-    totalAmount += amountFor(perf);
   }
 
-  result += `Amount owed is ${usd(totalAmount)}\n`;
+  result += `Amount owed is ${usd(totalAmount())}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
+
+  function totalAmount() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += amountFor(perf);
+    }
+    return result
+  }
 
   function totalVolumeCredits() {
     let result = 0;
