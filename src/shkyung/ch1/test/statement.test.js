6183fe0e7480b3ffefcd38c63dc3ef8b02d9989a
sunny kyung <shkyung@lunit.io>
2021-05-18 14:48:38 +0900

    change result in statement.test.js

--- src/shkyung/ch1/test/statement.test.js
+++ src/shkyung/ch1/test/statement.test.js
@@ -2,13 +2,7 @@ import invoices from "../data/invoices.json";
 import plays from "../data/plays.json";
 import statement from "../statement";
 
-const result = `Statement for BigCo
- Hamlet: $650.00 (55 seats)
- As You Like It: $580.00 (35 seats)
- Othello: $500.00 (40 seats)
-Amount owed is $1,730.00
-You earned 47 credits
-`;
+const result = `<h1>청구 내역 (고객명: BigCo)</h1>\n<table>\n<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr><tr><td>Hamlet</td><td>($0.55석)</td><td>$650.00</td></tr>\n<tr><td>As You Like It</td><td>($0.35석)</td><td>$580.00</td></tr>\n<tr><td>Othello</td><td>($0.40석)</td><td>$500.00</td></tr>\n</table>\n<p>총액: <em>$1,730.00</em></p>\n<p>적립 포인트: <em>47</em>점</p>\n`;
 
 test("statement result", () => {
   expect(statement(invoices[0], plays)).toBe(result);
