23907377480261d6017485260aa04573f1d50951
Jiyeon Seo <jyseo@lunit.io>
2021-05-26 21:28:08 +0900

    jiyeon | create ex01

---
+++ src/jiyeon/ch6/01_extract_function/ex01_basic.js
@@ -0,0 +1,23 @@
+function pringOwing(invoice) {
+  let outstanding = 0;
+
+  console.log("**** 고객 채무 ****");
+
+  // 미해결 채무(outstanding)을 계산한다.
+  for (const o of invoice.orders) {
+    outstanding += o.amount;
+  }
+
+  // 마감일(dueDate)을 기록한다.
+  const today = Clock.today;
+  invoice.dueDate = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() + 30
+  );
+
+  // 세부 사항을 출력한다.
+  console.log(`고객명: ${invoice.customer}`);
+  console.log(`채무액: ${outstanding}`);
+  console.log(`마감일: ${invoice.dueDate.toLacaleDateString()}`);
+}
--- src/jiyeon/ch6/ex01_basic.js
+++
@@ -1,7 +0,0 @@
-function pringOwing(invoice) {
-  let outstanding = 0;
-
-  console.log("**** 고객 채무 ****");
-
-  // 미해결 채무(outstanding) 을 계산한다.
-}
