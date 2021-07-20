c3c22f0614d908c7d0b879915b2e0f5498157a7e
skpark-lunit <skpark@lunit.io>
2021-07-20 21:43:14 +0900

    ch10: add Bird class

--- src/skpark/ch10/index.js
+++ src/skpark/ch10/index.js
@@ -33,3 +33,37 @@ function airSpeedVelocity(bird) {
       return null;
   }
 }
+
+class Bird {
+  constructor(birdObject) {
+    Object.assign(this, birdObject);
+  }
+
+  // 깃털 상태
+  get plumage() {
+    switch (this.type) {
+      case "유럽 제비":
+        return "보통이다";
+      case "아프리카 제비":
+        return this.numberOfCoconuts > 2 ? "지쳤다" : "보통이다";
+      case "노르웨이 파랑 앵무":
+        return bird.voltage > 100 ? "그을렸다" : "예쁘다";
+      default:
+        return "알 수 없다";
+    }
+  }
+
+  // 비행 속도
+  get airSpeedVelocity() {
+    switch (this.type) {
+      case "유럽 제비":
+        return 35;
+      case "아프리카 제비":
+        return 40 - 2 * this.numberOfCoconuts;
+      case "노르웨이 파랑 앵무":
+        return this.isNailed ? 0 : 10 + this.voltage / 20;
+      default:
+        return null;
+    }
+  }
+}
