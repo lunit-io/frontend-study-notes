0bf6f09034efafa1969c4c2d6629bae5db216ac3
skpark-lunit <skpark@lunit.io>
2021-07-20 21:57:31 +0900

    ch10: implement class method plumage,airSpeedVelocity

--- src/skpark/ch10/index.js
+++ src/skpark/ch10/index.js
@@ -1,39 +1,16 @@
 function plumages(birds) {
-  return new Map(birds.map((b) => [b.name, plumage(b)]));
+  return new Map(
+    birds.map((b) => createBird(b)).map((bird) => [bird.name, bird.plumage])
+  );
 }
 
 function speeds(birds) {
-  return new Map(birds.map((b) => [b.name, airSpeedVelocity(b)]));
+  return new Map(
+    birds
+      .map((b) => createBird(b))
+      .map((bird) => [bird.name, bird.airSpeedVelocity])
+  );
 }
-
-function plumage(bird) {
-  // 깃털 상태
-  switch (bird.type) {
-    case "유럽 제비":
-      return "보통이다";
-    case "아프리카 제비":
-      return bird.numberOfCoconuts > 2 ? "지쳤다" : "보통이다";
-    case "노르웨이 파랑 앵무":
-      return bird.voltage > 100 ? "그을렸다" : "예쁘다";
-    default:
-      return "알 수 없다";
-  }
-}
-
-function airSpeedVelocity(bird) {
-  // 비행 속도
-  switch (bird.type) {
-    case "유럽 제비":
-      return 35;
-    case "아프리카 제비":
-      return 40 - 2 * bird.numberOfCoconuts;
-    case "노르웨이 파랑 앵무":
-      return bird.isNailed ? 0 : 10 + bird.voltage / 20;
-    default:
-      return null;
-  }
-}
-
 class Bird {
   constructor(birdObject) {
     Object.assign(this, birdObject);
@@ -41,30 +18,12 @@ class Bird {
 
   // 깃털 상태
   get plumage() {
-    switch (this.type) {
-      case "유럽 제비":
-        return "보통이다";
-      case "아프리카 제비":
-        return this.numberOfCoconuts > 2 ? "지쳤다" : "보통이다";
-      case "노르웨이 파랑 앵무":
-        return bird.voltage > 100 ? "그을렸다" : "예쁘다";
-      default:
-        return "알 수 없다";
-    }
+    return "알 수 없다";
   }
 
   // 비행 속도
   get airSpeedVelocity() {
-    switch (this.type) {
-      case "유럽 제비":
-        return 35;
-      case "아프리카 제비":
-        return 40 - 2 * this.numberOfCoconuts;
-      case "노르웨이 파랑 앵무":
-        return this.isNailed ? 0 : 10 + this.voltage / 20;
-      default:
-        return null;
-    }
+    return null;
   }
 }
 
@@ -81,8 +40,32 @@ function createBird(bird) {
   }
 }
 
-class EuropeanSwallow extends Bird {}
+class EuropeanSwallow extends Bird {
+  get plumage() {
+    return "보통이다";
+  }
 
-class AfricanSwallow extends Bird {}
+  get airSpeedVelocity() {
+    return 35;
+  }
+}
 
-class NorwegianBlueParrot extends Bird {}
+class AfricanSwallow extends Bird {
+  get plumage() {
+    return this.numberOfCoconuts > 2 ? "지쳤다" : "보통이다";
+  }
+
+  get airSpeedVelocity() {
+    return 40 - 2 * this.numberOfCoconuts;
+  }
+}
+
+class NorwegianBlueParrot extends Bird {
+  get plumage() {
+    return this.voltage > 100 ? "그을렸다" : "예쁘다";
+  }
+
+  get airSpeedVelocity() {
+    return this.isNailed ? 0 : 10 + this.voltage / 20;
+  }
+}
