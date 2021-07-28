18f34e0488c4b3a46df950b676d7c93243b642c0
skpark-lunit <skpark@lunit.io>
2021-07-28 21:21:49 +0900

    ch12: 12-10 위임메서드를 기반 메서드의 확장 형태로 재호출

--- src/skpark/ch12/12.10.js
+++ src/skpark/ch12/12.10.js
@@ -7,6 +7,10 @@ class PremiumBookingDelegate {
   get hasTalkback() {
     return this._host._show.hasOwnProperty("talkback");
   }
+
+  extendBasePrice(base) {
+    return Math.round(base + this._extra.premiumFee);
+  }
 }
 
 export class Booking {
@@ -29,7 +33,9 @@ export class Booking {
   get basePrice() {
     let result = this._show.price;
     if (this.isPeakDay) result += Math.round(result * 0.15);
-    return result;
+    return this._premiumDelegate
+      ? this._premiumDelegate.extendBasePrice(result)
+      : result;
   }
 
   _bePremium(extra) {
@@ -41,14 +47,6 @@ export class PremiumBooking extends Booking {
   constructor(show, date, extra) {
     super(show, date);
     this._extra = extra;
-  }
-
-  get hasTalkback() {
-    return this._premiumDelegate.hasTalkback;
-  }
-
-  get basePrice() {
-    return Math.round(super.basePrice + this._extra.premiumFee);
   }
 
   get hasDinner() {
