b98a53b67dbb844bca8f668d95d5fa4f80287f65
skpark-lunit <skpark@lunit.io>
2021-07-28 15:40:52 +0900

    ch12: 12-1 init

---
+++ src/skpark/ch12/12.1.js
@@ -0,0 +1,17 @@
+export class Party {
+  constructor(monthlyCost) {
+    this.monthlyCost = monthlyCost;
+  }
+}
+
+export class Employee extends Party {
+  get annualCost() {
+    return this.monthlyCost * 12;
+  }
+}
+
+export class Department extends Party {
+  get totalAnnualCost() {
+    return this.monthlyCost * 12;
+  }
+}
---
+++ src/skpark/ch12/12.1.spec.js
@@ -0,0 +1,10 @@
+import { Employee, Department } from "./12.1";
+
+describe("메서드 올리기", () => {
+  it("works", () => {
+    const employee = new Employee(5);
+    const department = new Department(11);
+    expect(employee.annualCost).toBe(60);
+    expect(department.totalAnnualCost).toBe(132);
+  });
+});
