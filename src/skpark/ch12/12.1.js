d85188342f942cf34b0c565d54c6d7786008e423
skpark-lunit <skpark@lunit.io>
2021-07-28 15:42:51 +0900

    ch12: 12-1 서브 클래스 메서드를 슈퍼 클래스로 올리기

--- src/skpark/ch12/12.1.js
+++ src/skpark/ch12/12.1.js
@@ -2,16 +2,12 @@ export class Party {
   constructor(monthlyCost) {
     this.monthlyCost = monthlyCost;
   }
-}
 
-export class Employee extends Party {
   get annualCost() {
     return this.monthlyCost * 12;
   }
 }
 
-export class Department extends Party {
-  get annualCost() {
-    return this.monthlyCost * 12;
-  }
-}
+export class Employee extends Party {}
+
+export class Department extends Party {}
