1228b0babbed785bef6cf34c96f999d6e82ce845
skpark-lunit <skpark@lunit.io>
2021-07-28 19:25:50 +0900

    ch12: 12-6 나머지 서브 클래스 만들고, 슈퍼클래스의 type 게터 제거

--- src/skpark/ch12/12.6.js
+++ src/skpark/ch12/12.6.js
@@ -2,17 +2,12 @@ class Employee {
   constructor(name, type) {
     this.validateType(type);
     this._name = name;
-    this._type = type;
   }
 
   validateType(arg) {
     if (!["engineer", "manager", "salesperson"].includes(arg)) {
       throw new Error(`${arg}라는 직원 유형은 없습니다.`);
     }
-  }
-
-  get type() {
-    return this._type;
   }
 
   toString() {
@@ -26,10 +21,26 @@ class Engineer extends Employee {
   }
 }
 
+class Manager extends Employee {
+  get type() {
+    return "manager";
+  }
+}
+
+class Salesperson extends Employee {
+  get type() {
+    return "salesperson";
+  }
+}
+
 export function createEmployee(name, type) {
   switch (type) {
     case "engineer":
       return new Engineer(name, type);
+    case "manager":
+      return new Manager(name, type);
+    case "salesperson":
+      return new Salesperson(name, type);
   }
   return new Employee(name, type);
 }
