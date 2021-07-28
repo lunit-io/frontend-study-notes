0319a034cf4da712c951c3fca30c0fc2807769d6
skpark-lunit <skpark@lunit.io>
2021-07-28 18:51:39 +0900

    ch12: 12-3 공통 코드를 슈퍼클래스 생성자로 옮긴다

--- src/skpark/ch12/12.3.js
+++ src/skpark/ch12/12.3.js
@@ -1,4 +1,8 @@
 class Party {
+  constructor(name) {
+    this._name = name;
+  }
+
   get name() {
     return this._name;
   }
@@ -6,8 +10,7 @@ class Party {
 
 export class Employee extends Party {
   constructor(name, id, monthlyCost) {
-    super();
-    this._name = name;
+    super(name);
     this._id = id;
     this._monthlyCost = monthlyCost;
   }
@@ -19,8 +22,7 @@ export class Employee extends Party {
 
 export class Department extends Party {
   constructor(name, staff) {
-    super();
-    this._name = name;
+    super(name);
     this._staff = staff;
   }
 }
